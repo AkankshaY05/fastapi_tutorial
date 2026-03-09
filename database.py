65eb91592357c98c426d5ffe78be392a0e4b857f
AkankshaY05 <akanksha.yadav0820@gmail.com>
2026-03-09 20:13:34 +0530

    Fastapi with docker

--- database.py
+++ database.py
@@ -1,24 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
-from urllib.parse import quote_plus
- 
 
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = quote_plus(os.getenv("MYSQL_PASSWORD", "rootpassword"))
-MYSQL_HOST = os.getenv("MYSQL_HOST", "db")
-MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "fastapi_db")
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "Ak%40ietlko05"
+MYSQL_HOST = "localhost"
+MYSQL_PORT = "3306"
+MYSQL_DATABASE = "fastapi_db"
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
 ## Connection
 
-engine = create_engine(DATABASE_URL,
-                       echo = True,
-                       pool_pre_ping = True
-)
+engine = create_engine(DATABASE_URL)
 
 ## Session
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind = engine)
